a205760bb10891f42b0d6190c5fecd6fce5f24d3
20226026-dopg <phangiado1107@gmail.com>
2026-04-03 11:03:10 +0700

    feat: add constants and utilities for app structure
    - Created data constants for tabs, user info, home balance, upcoming subscriptions, and home subscriptions.
    - Introduced icon constants for easy access to image assets.
    - Added image constants for splash patterns and avatars.
    - Established a theme file with color and spacing definitions for consistent styling.
    - Added TypeScript declarations for image imports to support various formats.
    - Implemented utility functions for formatting currency, subscription dates, and status labels.
    - Defined global TypeScript interfaces for app tabs, subscriptions, and related components.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
 import { Stack } from "expo-router";
 import "@/global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "PlusJakartaSans-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "PlusJakartaSans-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "PlusJakartaSans-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "PlusJakartaSans-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "PlusJakartaSans-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "PlusJakartaSans-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (!fontsLoaded) {
+      console.warn("Fonts are not loaded yet!");
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null; // or a loading spinner
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
